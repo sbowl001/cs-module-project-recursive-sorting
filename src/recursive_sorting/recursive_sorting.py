51b7422917399b5f7635b7a6b3d5d370e33e78d1
Stephanie <sbowl001@fiu.edu>
2020-06-08 11:45:54 +0800

    saving

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -47,14 +47,37 @@ def merge_sort(arr):
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    start2 = mid + 1
 
+    if (arr[mid] <= arr[start2]):
+        return 
+    while (start <= mid and start2 <= end): 
+        if (arr[start] <= arr[start2]):
+            start += 1
+        else: 
+            value = arr[start2]
+            index = start2
+            while (index != start):
+                arr[index] = arr[index - 1]
+                index -= 1 
+            arr[start] = value 
+
+            start += 1
+            mid += 1
+            start2 += 1
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    if (l < r): 
+        m  = l + (r -1) // 2
 
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+
+        merge_in_place(arr, l, m, r)
 
     return arr
 
